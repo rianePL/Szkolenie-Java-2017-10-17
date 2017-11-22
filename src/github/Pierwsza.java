41fb7ce261709cc41ca2a36fa42eceec041ff0ba
kurs <kurs@kurs-Komputer>
2017-11-22 16:45:17 +0100

    DASDSA
    DASDASDASDASD

--- src/github/Pierwsza.java
+++ src/github/Pierwsza.java
@@ -3,7 +3,7 @@ package github;
 public class Pierwsza {
 
 	public static void main(String[] args) {
-		System.out.println("Wreszcie w chmurze :)");
+		System.out.println("Wresjiojijizcie w chmurze :)");
 
 	}
 
