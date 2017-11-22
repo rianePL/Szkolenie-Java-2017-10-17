3ca65bcc81feb720efa9857e329cb7c38c309f1d
kurs <kurs@kurs-Komputer>
2017-11-22 16:45:02 +0100

    Taka jazda

---
+++ src/github/Trelemorele.java
@@ -0,0 +1,19 @@
+package github;
+
+public class Trelemorele {
+
+	public static void main(String[] args) {
+		
+		
+		
+		int i = 0;
+		
+		for (i = 0; i < 20; i++) {
+			
+			i++;
+			
+		}
+
+	}
+
+}
