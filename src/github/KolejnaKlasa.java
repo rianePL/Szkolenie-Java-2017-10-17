061f3bf062e5ffbb08f1a17361fec3bf08fb13fd
kurs <kurs@kurs-Komputer>
2017-11-22 16:47:27 +0100

    Nowa Klasa

--- src/github/KolejnaKlasa.java
+++ src/github/KolejnaKlasa.java
@@ -3,7 +3,7 @@ package github;
 public class KolejnaKlasa {
 
 	public static void main(String[] args) {
-		System.out.println("Klasa od mbrateq");
+		System.out.println("Klasa od mbrateq!");
 
 	}
 
